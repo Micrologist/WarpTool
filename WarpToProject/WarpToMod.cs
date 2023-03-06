9b3b9c36e8f5d1f02dd7806a125f142dbdd09e9d
Micrologist <7757920+Micrologist@users.noreply.github.com>
2023-03-06 01:33:38 +0100

    maxratepatch + proof of concept

--- WarpToProject/WarpToMod.cs
+++ WarpToProject/WarpToMod.cs
@@ -1,4 +1,9 @@
 ﻿using BepInEx;
+using HarmonyLib;
+using KSP.Game;
+using KSP.Messages.PropertyWatchers;
+using KSP.Sim.impl;
+using KSP.UI.Binding;
 using SpaceWarp;
 using SpaceWarp.API.Assets;
 using SpaceWarp.API.Mods;
@@ -14,22 +19,31 @@ namespace WarpTo
 	{
 		private GUISkin skin;
 		private GUIStyle windowStyle;
+		private GUIStyle closeBtnStyle;
 		private bool showGUI;
 		private Rect mainGuiRect;
+		private Rect closeBtnRect;
+		private VesselComponent activeVessel;
+		private readonly float windowWidth = 250;
 
 		public override void OnInitialized()
 		{
-			Logger.LogInfo("Warp to loaded");
 			skin = Skins.ConsoleSkin;
 
 			windowStyle = new GUIStyle(skin.window)
 			{
 				padding = new RectOffset(8, 8, 20, 8),
 				contentOffset = new Vector2(0, -22),
-				fixedWidth = 250
+				fixedWidth = windowWidth
+			};
+
+			closeBtnStyle = new GUIStyle(skin.button)
+			{
+				fontSize = 8
 			};
 
 			mainGuiRect = new(Screen.width * 0.5f, Screen.height * 0.2f, 0, 0);
+			closeBtnRect = new Rect(windowWidth - 23, 6, 16, 16);
 
 			Appbar.RegisterAppButton(
 					"Warp To",
@@ -39,10 +53,17 @@ namespace WarpTo
 			);
 		}
 
+		private void Start()
+		{
+			new Harmony("warptopatch").PatchAll();
+		}
+
 		private void OnGUI()
 		{
-			if (!showGUI) return;
+			activeVessel = GameManager.Instance?.Game?.ViewController?.GetActiveVehicle(true)?.GetSimVessel(true);
+			if (!showGUI || activeVessel == null) return;
 
+			GUI.skin = skin;
 			mainGuiRect = GUILayout.Window(
 				GUIUtility.GetControlID(FocusType.Passive),
 				mainGuiRect,
@@ -51,11 +72,67 @@ namespace WarpTo
 				windowStyle,
 				GUILayout.Height(0)
 			);
+			mainGuiRect.position = ClampToScreen(mainGuiRect.position, mainGuiRect.size);
+
+
 		}
+
 
 		private void FillGUI(int id)
 		{
-			
+			if (CloseButton())
+			{
+				CloseWindow();
+			}
+			GUILayout.Space(10);
+			GUILayout.BeginHorizontal();
+			GUILayout.Label("Warp to Apoapsis");
+			if (GUILayout.Button("Click me")) WarpToApoapsis();
+			GUILayout.EndHorizontal();
+
+			GUI.DragWindow(new Rect(0, 0, 1000, 1000));
+		}
+
+		private void WarpToApoapsis()
+		{
+			double timeToApoapsis = activeVessel.Orbit.TimeToAp;
+			double warpTime = GameManager.Instance.Game.UniverseModel.UniversalTime + timeToApoapsis;
+			GameManager.Instance.Game.ViewController.TimeWarp.WarpTo(warpTime);
+		}
+
+		private Vector2 ClampToScreen(Vector2 position, Vector2 size)
+		{
+			float x = Mathf.Clamp(position.x, 0, Screen.width - size.x);
+			float y = Mathf.Clamp(position.y, 0, Screen.height - size.y);
+			return new Vector2(x, y);
+		}
+
+		private bool CloseButton()
+		{
+			return GUI.Button(closeBtnRect, "x", closeBtnStyle);
+		}
+
+		private void CloseWindow()
+		{
+			GameObject.Find("BTN-WarpToBtn")?.GetComponent<UIValue_WriteBool_Toggle>()?.SetValue(false);
+			showGUI = false;
+		}
+
+
+	}
+
+
+	[HarmonyPatch(typeof(TimeWarp))]
+	[HarmonyPatch("GetMaxRateIndex")]
+	class MaxTimeWarpPatch
+	{
+		static void Postfix(ref int __result)
+		{
+			VesselComponent activeVessel = GameManager.Instance?.Game?.ViewController?.GetActiveVehicle(true)?.GetSimVessel(true);
+			if(activeVessel != null && !activeVessel.IsInAtmosphere)
+			{
+				__result = PhysicsSettings.TimeWarpLevels.Length - 1;
+			}
 		}
 	}
 }
